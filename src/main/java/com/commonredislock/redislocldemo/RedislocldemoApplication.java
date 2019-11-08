0132e6c2b1e246b4a502b823bf72eff8c494a944
Zhaohongy <zhaohongyu@zhixueyun.com>
2019-11-08 14:34:31 +0800

    单机redis分布式锁

---
+++ src/main/java/com/commonredislock/redislocldemo/RedislocldemoApplication.java
@@ -0,0 +1,13 @@
+package com.commonredislock.redislocldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 教学链接  https://blog.csdn.net/weixin_38003389/article/details/89434629
+@SpringBootApplication
+public class RedislocldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedislocldemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/commonredislock/redislocldemo/RedislocldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonredislock.redislocldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedislocldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
